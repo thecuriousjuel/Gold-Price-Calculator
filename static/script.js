ed9f37eb7a0e466acb00efe06fbb608277e9d1b6
Biswajit Basak <juelbasak@gmail.com>
2025-06-15 20:23:43 +0530

    Fixing bug that empties the gold rate

--- static/script.js
+++ static/script.js
@@ -153,7 +153,6 @@ function loadMainPage() {
     clearButtonEventListener.addEventListener('click', (event) => {
         weightInput.value = '';
         makingChargeInput.value = '';
-        goldRateInput.value = '';
         document.querySelector('#result-container').innerHTML = '';
     });
 
